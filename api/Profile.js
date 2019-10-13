ff21caf2ad89c9960cb04ded9cf663cdc1c57b58
Tushar <wtushar09@gmail.com>
2019-10-13 22:18:28 +0600

    some text changed

--- api/Profile.js
+++ api/Profile.js
@@ -1,7 +1,7 @@
 var db = require('../src/db/connector');
 module.exports = {
     create: function (req, res) {
-        //var //
+        //var
         const payload = req.body;
         var query = `insert into users values(null, "${payload.name}", "${payload.email}", "${payload.phone.toString()}", null, null, now(), now())`;
         db.query(query, function (error, data) {
