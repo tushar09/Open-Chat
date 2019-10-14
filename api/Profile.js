05cd131429bce42aa226522f54e9141d71936c23
Tushar <wtushar09@gmail.com>
2019-10-14 14:13:57 +0600

    msg changed again

--- api/Profile.js
+++ api/Profile.js
@@ -6,14 +6,14 @@ module.exports = {
         var query = `insert into users values(null, "${payload.name}", "${payload.email}", "${payload.phone.toString()}", null, now(), now())`;
         db.query(query, function (error, data) {
             if (data == null) {
-                return res.send({ msg: "not created", success: false });
+                return res.send({ msg: "account not created", success: false });
             } else {
                 var passwordQuery = `insert into password values(null, "${data.insertId}", "${payload.pass}")`;
                 db.query(passwordQuery, function (error, data) {
                     if (data != null) {
                         return res.send({ msg: "account created successfully", success: true });
                     } else {
-                        return res.json({ msg: "not created", success: false });
+                        return res.send({ msg: "account not created", success: false });
                     }
                 })
 
