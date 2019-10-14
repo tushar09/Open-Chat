223d137bd236c6c0f7cd23c9340f73d28fc12505
Tushar <wtushar09@gmail.com>
2019-10-14 18:37:06 +0600

    disconnect event called

--- src/socket.js
+++ src/socket.js
@@ -1,11 +1,19 @@
+var db = require('../src/db/connector');
 module.exports = function(io) {
     const con = io.of('/con');
+    let user;
     con.on('connection', function(socket) {
         console.log('user connected' + socket.id);
-        socket.on('userInfo', function(user) {
+        socket.on('userInfo', function(obj) {
+            user = JSON.parse(obj);
+            const query = `UPDATE users SET socket_id = "${socket.id}" WHERE email = "${user.email}"`;
+            console.log(query);
+            db.query(query);
             console.log(user);
         });
         socket.on('disconnect', function(s) {
+            const query = `UPDATE users SET socket_id = null WHERE email = "${user.email}"`;
+            db.query(query);
             console.log("asdf" + socket.id);
         });
     });
