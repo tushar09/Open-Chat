fdf64698afd7e63153a0ffff452dca922424bd76
Tushar <wtushar09@gmail.com>
2019-10-15 12:15:10 +0600

    diagram added

--- src/socket.js
+++ src/socket.js
@@ -6,13 +6,13 @@ module.exports = function(io) {
         console.log('user connected' + socket.id);
         socket.on('userInfo', function(obj) {
             user = JSON.parse(obj);
-            const query = `UPDATE users SET socket_id = "${socket.id}" WHERE email = "${user.email}"`;
+            const query = `UPDATE users SET socket_id = "${socket.id}", updated_at = now() WHERE email = "${user.email}"`;
             console.log(query);
             db.query(query);
             console.log(user);
         });
         socket.on('disconnect', function(s) {
-            const query = `UPDATE users SET socket_id = null WHERE email = "${user.email}"`;
+            const query = `UPDATE users SET socket_id = null, updated_at = now() WHERE email = "${user.email}"`;
             db.query(query);
             console.log("asdf" + socket.id);
         });
