bb5e6d1c9030c4490b27468658f8b536458c8f47
Tushar <wtushar09@gmail.com>
2020-03-02 00:44:45 +0600

    msg db changed and query added

--- api/Message.js
+++ api/Message.js
@@ -6,6 +6,15 @@ var fcm = new FCM(constants.serverKey);
 module.exports = {
     sendMsg: function(req, res){
         const payLoad = req.body;
+
+        //const query = `select * from users where phone like "%${payLoad.phone}"`;
+        const query = `insert into topics values(null, "%${payLoad.phone}")`;
+        db.query(query, function(error, data){
+            if(error){
+                console.log(error);
+            }
+        });
+
         var message = {
             to: '/topics/'.concat(payLoad.topic),
             data:{
