72f5106230af8ddfbc0299d63a55330b219998de
Tushar <wtushar09@gmail.com>
2020-03-02 20:57:15 +0600

    duplicxate handle

--- api/Message.js
+++ api/Message.js
@@ -8,7 +8,7 @@ module.exports = {
         const payLoad = req.body;
 
         //const query = `select * from users where phone like "%${payLoad.phone}"`;
-        const query = `insert into topics values(null, "${payLoad.topic}")`;
+        const query = `insert into topics values(null, "${payLoad.topic}") on duplicate key update name = "${payLoad.topic}"`;
         db.query(query, function(error, data){
             if(error){
                 console.log(error);
