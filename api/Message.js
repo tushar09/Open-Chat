08609f287ecb58c0c524e91187d0419c8aa04270
Tushar <wtushar09@gmail.com>
2020-03-12 21:05:07 +0600

    response sent back

--- api/Message.js
+++ api/Message.js
@@ -77,7 +77,11 @@ module.exports = {
         }
         db.query(query, function(err, data){
         });
-
+        if(err){
+            res.send(err);
+        }else{
+            res.send(data);
+        }
         
     }
 }
